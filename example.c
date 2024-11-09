bc447ae952a7342732fcdff1bbc6424a8698835a
sungi871 <minsg3@gachon.ac.kr>
2024-11-09 22:46:46 +0900

    Create example.c

---
+++ example.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    FILE *inFile;
+    char line[100];
+    int lcount=0;
+
+    if((inFile=fopen("/Users/admin/Downloads/input_files/charstream.txt","r"))==NULL){
+        printf("File Could Not Be Opened.\n");
+        exit(1);
+    }
+    while (fgets(line,sizeof(line),inFile)>0){
+        lcount++;
+        printf("Line %d: %s",lcount,line);
+    }
+    fclose(inFile);
+    return 0;
+}
